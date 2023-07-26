7965a0c8e581e9118770b0f8d563894311966dfb
Jan Nöhles <jan.noehles@easybill.de>
2023-07-26 11:46:09 +0200

    Remove test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from easybill_rest import Client
-
-client = Client("u9NWgHLUwQWXNsJRq1l8dl5jdNGSOYCj6mI66yOgx2ETxYOeViCEEtaQPwulEhjP")
-result = client.documents().get_document("2509811976")
-print(result)
